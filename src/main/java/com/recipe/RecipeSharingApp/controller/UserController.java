4a9a037e64bc95e430e5e97f1f9acc2052dc3ce3
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-07 04:00:04 +0530

    add userDto

--- src/main/java/com/recipe/RecipeSharingApp/controller/UserController.java
+++ src/main/java/com/recipe/RecipeSharingApp/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.recipe.RecipeSharingApp.controller;
 
 import com.recipe.RecipeSharingApp.entities.User;
+import com.recipe.RecipeSharingApp.payload.UserDto;
 import com.recipe.RecipeSharingApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +29,24 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+    
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable("id") long id){
+    	return new ResponseEntity<>(userService.getById(id),HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or #id == authentication.principal.id")
+    public ResponseEntity<String> deleteById(@PathVariable("id") long id) {
+    	return new ResponseEntity<>(userService.deleteById(id),HttpStatus.OK);
+    }
+    
+    @PutMapping("/{id}")
+    @PreAuthorize("#id == authentication.principal.id")
+    public ResponseEntity<UserDto> updateById(@PathVariable("id")long id,@RequestBody UserDto userDto){
+    	return new ResponseEntity<>(userService.updateById(id,userDto),HttpStatus.OK);
     }
 
 }
