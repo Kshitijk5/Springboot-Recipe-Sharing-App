558ed7541fe8ee9fbcd311c7a84e43d5069590c2
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-06 17:42:43 +0530

    add whitelist fir /auth/** endpoint

--- src/main/java/com/recipe/RecipeSharingApp/security/SecurityConfig.java
+++ src/main/java/com/recipe/RecipeSharingApp/security/SecurityConfig.java
@@ -1,16 +1,30 @@
 package com.recipe.RecipeSharingApp.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Controller;
 
-public final class SecurityConfig {
+@Configuration
+@EnableMethodSecurity
+public  class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api/**").permitAll()
                                 .requestMatchers("/swagger-ui/index.html").permitAll()
 
 //                        .requestMatchers("/swagger-resources/", "/webjars/")
@@ -20,5 +34,15 @@ public final class SecurityConfig {
 
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
