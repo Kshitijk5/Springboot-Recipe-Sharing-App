bbc6e3df32e6d6080b791e6f62fd3e4456b99887
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-07 00:35:31 +0530

    add register and login functionality

--- src/main/java/com/recipe/RecipeSharingApp/service/AuthService.java
+++ src/main/java/com/recipe/RecipeSharingApp/service/AuthService.java
@@ -1,7 +1,7 @@
 package com.recipe.RecipeSharingApp.service;
 
-import com.recipe.RecipeSharingApp.dto.Login;
-import com.recipe.RecipeSharingApp.dto.Register;
+import com.recipe.RecipeSharingApp.payload.Login;
+import com.recipe.RecipeSharingApp.payload.Register;
 
 public interface AuthService {
 
