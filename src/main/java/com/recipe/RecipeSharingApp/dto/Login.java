685483e15a195a7f6e3b286ea236aa415e8380a3
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-06 17:45:45 +0530

    add login dto

---
+++ src/main/java/com/recipe/RecipeSharingApp/dto/Login.java
@@ -0,0 +1,16 @@
+package com.recipe.RecipeSharingApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Login {
+
+    private String usernameOrEmail;
+    private String password;
+}
