e5cbc3e9de7f6884bf193003a7a2904ba60ec330
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-07 00:38:36 +0530

    add @JsonIgnore

--- src/main/java/com/recipe/RecipeSharingApp/entities/Difficulty.java
+++ src/main/java/com/recipe/RecipeSharingApp/entities/Difficulty.java
@@ -1,6 +1,7 @@
 package com.recipe.RecipeSharingApp.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,6 +22,7 @@ public class Difficulty {
     private long id;
     private String difficultyLevel;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "difficultyLevel")
     private List<Recipe> recipes;
 
