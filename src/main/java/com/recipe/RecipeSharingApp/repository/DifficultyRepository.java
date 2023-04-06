fe68ce7ffcda461c667b71280542209ddacc1eb9
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-07 00:39:12 +0530

    Add findByName

--- src/main/java/com/recipe/RecipeSharingApp/repository/DifficultyRepository.java
+++ src/main/java/com/recipe/RecipeSharingApp/repository/DifficultyRepository.java
@@ -3,5 +3,9 @@ package com.recipe.RecipeSharingApp.repository;
 import com.recipe.RecipeSharingApp.entities.Difficulty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DifficultyRepository extends JpaRepository<Difficulty,Long> {
+
+    Optional<Difficulty> findByDifficultyLevel(String diffcultyLevel);
 }
