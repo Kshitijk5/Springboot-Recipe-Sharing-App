7754e2c7ca2749d0928d13ee9801b0f8ba574e62
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-07 00:39:40 +0530

    Add GlobalException

---
+++ src/main/java/com/recipe/RecipeSharingApp/exception/GlobalException.java
@@ -0,0 +1,33 @@
+package com.recipe.RecipeSharingApp.exception;
+
+import com.recipe.RecipeSharingApp.payload.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(RecipeAPIException.class)
+    public ResponseEntity<ErrorDetails> exceptionHandler(RecipeAPIException recipeAPIException, WebRequest webrequest) {
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), recipeAPIException.getMessage(), webrequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,
+                                                              WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
