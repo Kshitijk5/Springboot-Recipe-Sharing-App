04dd51232ad9027f8969413520f838996dae1dce
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-07 04:01:59 +0530

    add user service implementation

--- src/main/java/com/recipe/RecipeSharingApp/service/Impl/UserServiceImpl.java
+++ src/main/java/com/recipe/RecipeSharingApp/service/Impl/UserServiceImpl.java
@@ -1,23 +1,67 @@
 package com.recipe.RecipeSharingApp.service.Impl;
 
 import com.recipe.RecipeSharingApp.entities.User;
+import com.recipe.RecipeSharingApp.exception.ResourceNotFoundException;
 import com.recipe.RecipeSharingApp.repository.UserRepository;
 import com.recipe.RecipeSharingApp.service.UserService;
+import com.recipe.RecipeSharingApp.payload.UserDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserServiceImpl  implements UserService {
+public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @Override
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
+	@Autowired
+	private ModelMapper modelMapper;
 
+	@Override
+	public List<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+
+	@Override
+	public User getById(long id) {
+		return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User", "Id", id));
+
+	}
+
+	@Override
+	public String deleteById(long id) {
+		User user = getById(id);
+
+		userRepository.delete(user);
+		return "User deleted";
+
+	}
+
+	@Override
+	public UserDto updateById(long id, UserDto userDto) {
+		User user = mapToEntity(userDto);
+
+		User tempUser = getById(id);
+
+		tempUser.setUsername(userDto.getUsername());
+		tempUser.setPassword(user.getPassword());
+		tempUser.setEmail(user.getEmail());
+
+//		userRepository.save(tempUser);
+
+		return mapToDto(userRepository.save(tempUser));
+	}
+
+	private User mapToEntity(com.recipe.RecipeSharingApp.payload.UserDto dto) {
+		return modelMapper.map(dto, User.class);
+	}
+
+	private UserDto mapToDto(User user) {
+		return modelMapper.map(user, UserDto.class);
+	}
 
 }
