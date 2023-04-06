bf859654ac711a491b2cfa3edbbf39dd69feafe9
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-07 04:01:42 +0530

    add user service

--- src/main/java/com/recipe/RecipeSharingApp/service/UserService.java
+++ src/main/java/com/recipe/RecipeSharingApp/service/UserService.java
@@ -1,10 +1,17 @@
 package com.recipe.RecipeSharingApp.service;
 
 import com.recipe.RecipeSharingApp.entities.User;
+import com.recipe.RecipeSharingApp.payload.UserDto;
 
 import java.util.List;
 
 public interface UserService {
 
     List<User> getAllUsers();
+
+	User getById(long id);
+
+	String deleteById(long id);
+
+	UserDto updateById(long id, UserDto userDto);
 }
