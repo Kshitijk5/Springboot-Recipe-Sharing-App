66280ab8b5a58370ad5afedf817055b521894c37
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-07 00:40:09 +0530

    add @JsonIgnore

--- src/main/java/com/recipe/RecipeSharingApp/entities/Recipe.java
+++ src/main/java/com/recipe/RecipeSharingApp/entities/Recipe.java
@@ -1,10 +1,7 @@
 package com.recipe.RecipeSharingApp.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.yaml.snakeyaml.events.Event;
 
 import java.time.Duration;
@@ -16,6 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@ToString
 public class Recipe {
 
      @Id
