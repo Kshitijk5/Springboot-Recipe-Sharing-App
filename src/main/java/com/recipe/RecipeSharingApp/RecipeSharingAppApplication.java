b94224f915c296111872ecf986eaed07ab45f860
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-07 00:42:19 +0530

    add getModelMapper bean

--- src/main/java/com/recipe/RecipeSharingApp/RecipeSharingAppApplication.java
+++ src/main/java/com/recipe/RecipeSharingApp/RecipeSharingAppApplication.java
@@ -1,13 +1,20 @@
 package com.recipe.RecipeSharingApp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RecipeSharingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeSharingAppApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper(){
+		return new ModelMapper();
 	}
 
 }
