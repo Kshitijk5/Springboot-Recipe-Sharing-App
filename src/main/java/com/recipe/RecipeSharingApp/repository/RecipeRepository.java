6415da1e2540ec04943a46991b20b8ba3f9169fc
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-07 15:50:38 +0530

    add findByUser(User user)

--- src/main/java/com/recipe/RecipeSharingApp/repository/RecipeRepository.java
+++ src/main/java/com/recipe/RecipeSharingApp/repository/RecipeRepository.java
@@ -1,13 +1,17 @@
 package com.recipe.RecipeSharingApp.repository;
 
 import com.recipe.RecipeSharingApp.entities.Recipe;
+import com.recipe.RecipeSharingApp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RecipeRepository extends JpaRepository<Recipe,Long> {
     @Override
     Optional<Recipe> findById(Long aLong);
+
+    List<Recipe> findByUser(User user);
 
 
 }
